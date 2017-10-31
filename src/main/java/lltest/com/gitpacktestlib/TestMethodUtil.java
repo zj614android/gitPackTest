917674709756be5c80acdff6b6ac57184c9cf333
zhujiao <410799459@qq.com>
2017-10-31 14:52:40 +0800

    method 1 commit

---
+++ src/main/java/lltest/com/gitpacktestlib/TestMethodUtil.java
@@ -0,0 +1,15 @@
+package lltest.com.gitpacktestlib;
+
+import android.util.Log;
+
+/**
+ * Created by thinkpad on 2017/10/31.
+ */
+
+public class TestMethodUtil {
+
+    public static void test1(){
+        Log.e("test","test1");
+    }
+
+}
